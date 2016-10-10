348ecc95ea542f81b1649faa3f4f6c8da2988601
Janik Vonrotz <login@janikvonrotz.ch>
2016-10-10 09:58:19 -0700

    update readme

--- imports/config.js
+++ imports/config.js
@@ -1,8 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { objectAssign } from './helper';
 
-// extract meteor settings
-// var config = Object.assign(Meteor.settings.private, Meteor.settings.public);
 var config = objectAssign(Meteor.settings.private, Meteor.settings.public)
 
 export default config
